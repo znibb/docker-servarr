7be8af4480964969ada08c44ddee768c4232cff2
znibb <znibb@zkylark.se>
2024-06-20 15:51:13 +0200

    Moved example files to subdirectory

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ ! -f .env ]; then
-	cp .env.example .env
+	cp files/.env.example .env
 fi
 if [ ! -f transmission_password.secret ]; then
-	cp transmission_password.secret.example transmission_password.secret
+	cp files/transmission_password.secret.example transmission_password.secret
 fi
